df3cff074663f379dd0e41240e032cd351b513cf
swcraig <swcraig.code@gmail.com>
2018-03-19 22:51:21 -0700

    Add the script to rule them all
    This tells me to take a hike every 20 minutes.

---
+++ assets/site.js
@@ -0,0 +1,15 @@
+Notification.requestPermission();
+var call = true;
+
+setInterval(function() {
+  var now = new Date().getMinutes();
+
+  if ((now % 49 === 0) && call) {
+    var no = new Notification("Eye Strain Debugger", {
+      body: "Take a hike kiddo.",
+    });
+    call = false;
+  } else if (now % 20 !== 0) {
+    call = true;
+  }
+}, 15000);
